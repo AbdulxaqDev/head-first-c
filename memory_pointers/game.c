f4118219d1df28df98425cfdde5e64b07349a8a3
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-05-01 17:06:50 +0300

    feat: use pointer

--- memory_pointers/game.c
+++ memory_pointers/game.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-void go_south_east(int, int);
+void go_south_east(int *, int *);
 
 int main()
 {
     int latitude = 32;
     int longtitude = -64;
 
-    go_south_east(latitude, longtitude);
-    printf("Asast! Now at [%i, %i]\n", latitude, longtitude);
+    go_south_east(&latitude, &longtitude);
+    printf("Avast! Now at: [%i, %i]\n", latitude, longtitude);
+
+    int x = 4;
+    int *address_of_x = &x;
+    int value_of_x = *address_of_x;
+    printf("address of x: %p\n", address_of_x);
+    printf("value of x: %i\n", value_of_x);
+
     return 0;
 }
 
-void go_south_east(int lat, int lon)
+void go_south_east(int *lat, int *lon)
 {
-    lat--;
-    lon++;
-    printf("Asast! Changing to [%i, %i]\n", lat, lon);
+    *lat = *lat-1;
+    *lon = *lon+1;
 }
