c314324fbcb6e605cc3cda924f5a8e3f7dfd3d3a
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-05-02 18:48:25 +0300

    feat: readonly memory

--- memory_pointers/data_entry.c
+++ memory_pointers/data_entry.c
@@ -2,19 +2,36 @@
 
 int main()
 {
-    char name[40];
-    printf("Enter your name: ");
-    scanf("%39s", name);
 
-    int age;
-    printf("Enter your age: ");
-    scanf("%i", &age);
+    // scanf("", address)
+    // char name[40];
+    // printf("Enter your name: ");
+    // scanf("%39s", name);
 
-    char first_name[20];
-    char last_name[20];
-    printf("Enter your first and last name: ");
-    scanf("%19s %19s", first_name, last_name);
-    printf("First: %s, Last: %s\n", first_name, last_name);
+    // int age;
+    // printf("Enter your age: ");
+    // scanf("%i", &age);
+
+    // char first_name[20];
+    // char last_name[20];
+    // printf("Enter your first and last name: ");
+    // scanf("%19s %19s", first_name, last_name);
+    // printf("First: %s, Last: %s\n", first_name, last_name);
+
+    // fgets()
+    // char food[5];
+    // printf("Enter favorite food: ");
+    // fgets(food, sizeof(food), stdin);
+    // printf("%s\n", food);
+
+    char *cards = "JQK";
+    char a_card = cards[2];
+    cards[2] = cards[1];
+    cards[1] = cards[0];
+    cards[0] = cards[2];
+    cards[2] = cards[1];
+    cards[1] = a_card;
+    puts(cards);
 
     return 0;
 }
