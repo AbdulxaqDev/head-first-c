838137725e5bef2c83f304e282b7abe4a23e8463
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-05-17 20:06:33 +0300

    feat: getops

---
+++ files/argc_argv/pizza.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+
+int main(int argc, char *argv[])
+{
+    char *delivery = "";
+    int thick = 0;
+    int count = 0;
+    char ch;
+
+    while ((ch = getopt(argc, argv, "d:t")) != EOF)
+        switch (ch)
+        {
+        case 'd':
+            delivery = optarg;
+            break;
+        case 't':
+            thick = 1;
+            break;
+        defautl:
+            fprintf(stderr, "Unknown option: '%s'\n", optarg);
+            return 1;
+        }
+
+    // decrimenting the argc to skip the first argumant of command line arg
+    // incrementing the argv, to skip the first CLI argument, which is the file name
+    argc -= optind;
+    argv += optind;
+
+    if (thick)
+        puts("Thicl crust.");
+
+    if (delivery[0])
+        printf("To be delevired %s. \n", delivery);
+
+    puts("Ingredients: ");
+
+    for (count = 0; count < argc; count++)
+    {
+        puts(argv[count]);
+    }
+
+    return 0;
+}
--- files/argc_argv/unistd.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main()
-{
-
-    
-
-    return 0;
-}
