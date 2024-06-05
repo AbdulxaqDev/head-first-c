6919a8d0f463c0843d8ffa8b47bbb2b6688d408f
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-06-05 11:21:59 +0300

    feat: linked list

---
+++ dts_dynamic_mem/linked.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct island
+{
+    char *name;
+    char *opens;
+    char *closes;
+    struct island *next;
+} island;
+
+int main()
+{
+    island amity = {"Amity", "09:00", "17:00", NULL};
+    island craggy = {"Craggy", "09:00", "17:00", NULL};
+    island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
+    island shutter = {"shutter", "09:00", "17:00", NULL};
+
+    amity.next = &craggy;
+    craggy.next = &isla_nublar;
+    isla_nublar.next = &shutter;
+
+    
+
+
+    return 0;
+}
